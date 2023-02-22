d394343f36b44dafe175b5437d67ec156da7def3
Mike San Martin <mikeemmanuel91@gmail.com>
2023-02-22 20:04:59 -0300

    improve contact page

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -1,9 +1,47 @@
 import Footer from "../footer/Footer"
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { InstagramURL } from "../../features/URLS";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import EmailIcon from '@mui/icons-material/Email';
 
 const ContactComponent = () => {
+
+    console.log(process.env.REACT_APP_INSTAGRAM_URL)
     return(
         <div>
             <h1>Contacto Ulises Navas</h1>
+            <ul>
+                <li>
+                    <Link to={InstagramURL}>
+                        <Button variant="contained">
+                            <InstagramIcon/>
+                            <p>Instagram</p>
+                        </Button>
+                    </Link>
+                </li>
+                <li>
+                    <Button variant="contained">
+                        <YouTubeIcon/>  
+                        <p>YouTube</p>      
+                    </Button>
+                </li>
+                <li>
+                    <Button variant="contained">
+                        <WhatsAppIcon/>
+                        <p>Whatsapp</p>
+                    </Button>
+                </li>
+                <li>
+                    <Button variant="contained">
+                        <EmailIcon/>
+                        <p>Email</p>
+                    </Button>
+                </li>
+            </ul>
+            
             <Footer/>
         </div>
     )
---
+++ src/features/URLS.js
@@ -0,0 +1,4 @@
+export const InstagramURL = process.env.REACT_APP_INSTAGRAM_URL
+export const YouTubeURL = process.env.REACT_APP_YOUTUBE_URL
+export const WhatsappURL = process.env.REACT_APP_WHATSAPP_URL
+export const EmailURL = process.env.REACT_APP_EMAIL_URL
