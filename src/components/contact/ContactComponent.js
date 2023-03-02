5a3eec92743634f0a40ecfc0ad81e3100938e75b
Mike San Martin <mikeemmanuel91@gmail.com>
2023-03-02 16:35:50 -0300

    added links to contact page

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -1,7 +1,6 @@
 import Footer from "../footer/Footer"
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import { InstagramURL } from "../../features/URLS";
+import { InstagramURL, YouTubeURL, WhatsappURL, EmailURL } from "../../features/URLS";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
@@ -9,36 +8,41 @@ import EmailIcon from '@mui/icons-material/Email';
 
 const ContactComponent = () => {
 
-    console.log(process.env.REACT_APP_INSTAGRAM_URL)
     return(
         <div>
             <h1>Contacto Ulises Navas</h1>
             <ul>
                 <li>
-                    <Link to={InstagramURL}>
+                    <a href={InstagramURL}>
                         <Button variant="contained">
                             <InstagramIcon/>
                             <p>Instagram</p>
                         </Button>
-                    </Link>
+                    </a>
                 </li>
                 <li>
-                    <Button variant="contained">
-                        <YouTubeIcon/>  
-                        <p>YouTube</p>      
-                    </Button>
+                    <a href={YouTubeURL}>
+                        <Button variant="contained">
+                            <YouTubeIcon/>  
+                            <p>YouTube</p>      
+                        </Button>
+                    </a>
                 </li>
                 <li>
-                    <Button variant="contained">
-                        <WhatsAppIcon/>
-                        <p>Whatsapp</p>
-                    </Button>
+                    <a href={WhatsappURL}>
+                        <Button variant="contained">
+                            <WhatsAppIcon/>
+                            <p>Whatsapp</p>
+                        </Button>
+                    </a>
                 </li>
                 <li>
-                    <Button variant="contained">
-                        <EmailIcon/>
-                        <p>Email</p>
-                    </Button>
+                    <a href={EmailURL}>
+                        <Button variant="contained">
+                            <EmailIcon/>
+                            <p>Email</p>
+                        </Button>
+                    </a>
                 </li>
             </ul>
             
