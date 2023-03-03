20a295f2ec4d6bb915183a0ffb0c0a2769f6437c
Mike San Martin <mikeemmanuel91@gmail.com>
2023-03-03 15:25:50 -0300

    improve position of the footer and added contact section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,21 @@
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
     return(
         <div>
-            <h5>Ulises Navas</h5>
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                <BottomNavigation sx={{ width: 500 }}>
+                    <h5>Ulises Navas</h5><hr/>
+                    <h5>Contactame</h5>
+                    <Link to="/contact">
+                        <BottomNavigationAction label="Contacto" value="contact" icon={<ConnectWithoutContactIcon color='info' fontSize='large'/>}/>
+                    </Link>
+                </BottomNavigation>
+            </Paper>
         </div>
     )
 }
